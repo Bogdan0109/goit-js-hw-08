abb34d44f4615f03b0fab963221ffb6774585e75
Bogdan0109 <102528317+Bogdan0109@users.noreply.github.com>
2022-08-20 12:24:55 +0000

    Deploying to gh-pages from @ Bogdan0109/goit-js-hw-08@ec45c919dea160e720d17fb32717ee37bc51ce62 🚀

---
+++ partials/03-feedback.ceb9131f.js
@@ -0,0 +1,2 @@
+console.log("IGIGIGIGIGIUGIUG");
+//# sourceMappingURL=03-feedback.ceb9131f.js.map
---
+++ partials/03-feedback.dcbd50f3.js
@@ -0,0 +1,2 @@
+console.log("IGIGIGIGIGIUGIUG");
+//# sourceMappingURL=03-feedback.dcbd50f3.js.map
